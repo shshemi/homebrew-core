d280662af0e1c3e2e186cf87f2c9257d0e169be6
Michael Cho <michael@michaelcho.dev>
2024-10-07 14:13:59 -0400

    spidermonkey: revision bump to migrate to `icu4c@76`

--- Formula/s/spidermonkey.rb
+++ Formula/s/spidermonkey.rb
@@ -5,6 +5,7 @@ class Spidermonkey < Formula
   version "128.3.1"
   sha256 "c1f4052f3a88d96a122551d5025053304007f7649886d5e2fdfd1a11ce3d70a8"
   license "MPL-2.0"
+  revision 1
   head "https://hg.mozilla.org/mozilla-central", using: :hg
 
   # Spidermonkey versions use the same versions as Firefox, so we simply check
@@ -27,7 +28,7 @@ class Spidermonkey < Formula
   depends_on "pkg-config" => :build
   depends_on "python@3.13" => :build
   depends_on "rust" => :build
-  depends_on "icu4c@75"
+  depends_on "icu4c@76"
   depends_on "nspr"
   depends_on "readline"
 
