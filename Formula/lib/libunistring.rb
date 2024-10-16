ba456c68320f02152caa3ba1511d6490aaf6cd29
Rui Chen <rui@chenrui.dev>
2024-10-16 19:22:40 -0400

    libunistring 1.3
    Co-authored-by: Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libunistring.rb
+++ Formula/lib/libunistring.rb
@@ -1,10 +1,10 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz"
-  sha256 "fd6d5662fa706487c48349a758b57bc149ce94ec6c30624ec9fdc473ceabbc8e"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.3.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.gz"
+  sha256 "8ea8ccf86c09dd801c8cac19878e804e54f707cf69884371130d20bde68386b7"
   license any_of: ["GPL-2.0-only", "LGPL-3.0-or-later"]
 
   bottle do
@@ -18,12 +18,18 @@ class Libunistring < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "416da7b6b8d158de4adccc0479df3b8cc6a532f39f71df928dd979b01812da21"
   end
 
+  on_sonoma :or_newer do
+    # gnulib rejects new macOS iconv implementation.
+    # https://github.com/coreutils/gnulib/commit/bab130878fe57086921fa7024d328341758ed453
+    depends_on "libiconv"
+  end
+
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    args = ["--disable-silent-rules"]
+    args << "--with-libiconv-prefix=#{Formula["libiconv"].opt_prefix}" if OS.mac? && MacOS.version >= :sonoma
+    system "./configure", *args, *std_configure_args
     system "make"
-    system "make", "check" if !OS.mac? || MacOS.version < :sonoma
+    system "make", "check"
     system "make", "install"
   end
 
