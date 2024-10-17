bd89e98270470036df08fa3472dff55f043f8111
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-17 15:24:41 +0000

    auditbeat 8.15.3

--- Formula/a/auditbeat.rb
+++ Formula/a/auditbeat.rb
@@ -2,8 +2,8 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.15.2",
-      revision: "26daf71e4ec87172523af7f0e916cba9f79dc0d0"
+      tag:      "v8.15.3",
+      revision: "bbed3ae55602e83f57c62de85b57a3593aa49efa"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
